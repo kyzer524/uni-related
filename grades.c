ad7153ae5c0b5d90e8b9008343567b0190759b5d
kyzer1415 <58799673+kyzer524@users.noreply.github.com>
2021-10-10 22:49:40 +0100

    Update grades.c

--- grades.c
+++ grades.c
@@ -8,23 +8,18 @@ int main()
   printf("Please enter your marks: \n");
   scanf("%i", &marks);
 
-  if (marks > 89 && marks < 101)
-    grade = 'A'
-
-  elif (marks > 79 && marks < 90)
-    grade = 'B'
-
-  elif (marks > 69 && marks < 80)
-    grade = 'C'
-
-  elif (marks > 59 && marks < 70)
-    grade = 'D'
-
-  elif (marks > 49 && marks < 60)
-    grade = 'E'
-
-  elif (marks < 50)
-    grade = 'F'
+  if (marks > 89 && marks < 101);
+    grade = 'A';
+  elif (marks > 79 && marks < 90);
+    grade = 'B';
+  elif (marks > 69 && marks < 80);
+    grade = 'C';
+  elif (grades > 59 && grades < 70);
+    grade = 'D';
+  elif (marks > 49 && marks < 60);
+    grade = 'E';
+  elif (marks < 50);
+    grade = 'F';
 
 
     switch(grade)
